140695f6465ec64b4005e8b12a7d2f797ca3a0bd
Ak-Yadav <akashyadav.htw@gmail.com>
2026-04-26 01:38:52 +0530

    feat: implement main application entry point and update dev scripts for concurrent client-server execution

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -74,7 +74,7 @@ function App() {
         <Sidebar onNavigate={navigateTo} currentPage={currentPage} />
 
         {/* The Rounded "Pod" (Main Content area) */}
-        <main className="flex-1 bg-vibaura-view-bg rounded-[40px] overflow-hidden flex flex-col mr-6 ml-2 py-6">
+        <main className="flex-1 bg-vibaura-view-bg rounded-[40px] overflow-hidden flex flex-col mr-6 ml-2 pb-6 pt-0">
           <div className="flex-1 overflow-y-auto no-scrollbar page-scroll-area relative">
 
             {/* Modular View Management */}
